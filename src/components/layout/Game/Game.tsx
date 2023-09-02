29ca348eb828ca6b74ccd934101bb6fa3d343881
Asher <asherplotnik@gmail.com>
2023-09-02 20:57:28 +0300

    movment

--- src/components/layout/Game/Game.tsx
+++ src/components/layout/Game/Game.tsx
@@ -9,24 +9,30 @@ interface ScreenPosition {
 const Game = () => {
   const [mousePosition, setMousePosition] = useState<ScreenPosition>({ x: 0, y: 0 });
   const [divPosition, setDivPosition] = useState<ScreenPosition>({ x: 0, y: 0 });
-  const [screenSize, setScreenSize] = useState<ScreenPosition>({ x: window.innerWidth, y: window.innerHeight});
+  const [screenSize, setScreenSize] = useState<ScreenPosition>({ x: window.innerWidth, y: window.innerHeight });
+  const [diskPosition, setDiskPosition] = useState<ScreenPosition>({ x: window.innerWidth / 2, y: window.innerHeight / 1.3, });
+  const [diskSpeed, setDiskSpeed] = useState<ScreenPosition>({ x:0, y:0 });
   const handleMouseMove = (e: MouseEvent) => {
     setMousePosition({ x: e.clientX, y: e.clientY });
   };
 
   const handleResize = () => {
-    setScreenSize({ x: window.innerWidth, y: window.innerHeight})
-  }
+    setScreenSize({ x: window.innerWidth, y: window.innerHeight });
+  };
 
   useEffect(() => {
-    const updateDivPosition = () => {
-      setDivPosition({ 
-            x: updateNewX(),
-            y: updateNewY() 
-        });
+    const updatePositions = () => {
+      setDivPosition({
+        x: updateMouseNewX(),
+        y: updateMouseNewY(),
+      });
+      setDiskPosition({
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 1.3,
+      });
     };
 
-    updateDivPosition();
+    updatePositions();
 
     window.addEventListener("mousemove", handleMouseMove);
     window.addEventListener("resize", handleResize);
@@ -35,47 +41,62 @@ const Game = () => {
     };
   }, [mousePosition]);
 
-const updateNewX = ():number => {
-    const maxX = screenSize.x/2 + screenSize.y*0.6/2;
-    const minX = screenSize.x/2 - screenSize.y*0.6/2;
-    if (mousePosition.x > minX && mousePosition.x < maxX){
-        return mousePosition.x - screenSize.y/40;
+  const updateMouseNewX = (): number => {
+    const maxX = screenSize.x / 2 + (screenSize.y * 0.3);
+    const minX = screenSize.x / 2 - (screenSize.y * 0.3);
+    if (mousePosition.x > minX && mousePosition.x < maxX) {
+      return mousePosition.x - screenSize.y / 40;
     }
 
     if (mousePosition.x > maxX) {
-        return maxX - screenSize.y/40;
+      return maxX - screenSize.y / 40;
     }
     if (mousePosition.x < minX) {
-        return minX - screenSize.y/40;
+      return minX - screenSize.y / 40;
     }
-    
-    return divPosition.x;
-}
+    return diskPosition.x;
+  };
 
-const updateNewY = ():number => {
-    const minY = screenSize.y / 2 + screenSize.y/26;
-    const maxY = screenSize.y - screenSize.y/26;
-    if (mousePosition.y > minY && mousePosition.y < maxY){
-        return mousePosition.y - screenSize.y/40;
+  const updateMouseNewY = (): number => {
+    const minY = screenSize.y / 2 + screenSize.y / 30;
+    const maxY = screenSize.y - screenSize.y / 30;
+    if (mousePosition.y > minY && mousePosition.y < maxY) {
+      return mousePosition.y - screenSize.y / 40;
     }
     if (mousePosition.y > maxY) {
-        return maxY - screenSize.y/40;
+      return maxY - screenSize.y / 40;
     }
     if (mousePosition.y < minY) {
-        return minY - screenSize.y/40;
+      return minY - screenSize.y / 40;
+    }
+    return diskPosition.y;
+  };
+
+  const listenImpactDisk = () => {
+    if (false) {
+
     }
 
-    return divPosition.y;
-}
+  }
 
-
-
+  listenImpactDisk();
+  const adjuster1 = 79;
   return (
     <div className={classes.Game}>
+        <div className={classes.PositionPlayer}>player1: ( {Math.round(divPosition.x,1)} | {Math.round(divPosition.y,1)} )</div>
+        <div className={classes.PositionDisk}>
+            disk: ( {Math.round(diskPosition.x,1)} | {Math.round(diskPosition.y,1)} )
+            <br />
+            <br />
+            screen size y : {screenSize.y}
+        </div>
       <div className={classes.Goal1}></div>
       <div className={classes.Goal2}></div>
       <div className={classes.Table}>
-        <div className={classes.Disk}></div>
+        <div
+          className={classes.Disk}
+          style={{ left: diskPosition.x, top: diskPosition.y }}
+        ></div>
         <div className={classes.Area1}>
           <div className={classes.Player1}></div>
         </div>
@@ -84,6 +105,14 @@ const updateNewY = ():number => {
             className={classes.Player2}
             style={{ left: divPosition.x, top: divPosition.y }}
           ></div>
+          <div
+            className={classes.Player2Mid}
+            style={{ left: divPosition.x + screenSize.y / adjuster1, top: divPosition.y + screenSize.y / adjuster1}}
+          ></div>
+          <div
+            className={classes.Player2Center}
+            style={{ left: divPosition.x + screenSize.y / 41, top: divPosition.y + screenSize.y / 41}}
+          ></div>
         </div>
       </div>
     </div>
@@ -91,5 +120,3 @@ const updateNewY = ():number => {
 };
 
 export default Game;
-
-
